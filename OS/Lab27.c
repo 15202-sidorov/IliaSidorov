4de5f05ac35b4190c0a05071ab88eb2e989977d0
ilia <iliasidorov90@gmail.com>
2017-04-22 00:41:20 +0700

    Lab27 is ready

---
+++ OS/Lab27.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#define CHUNCK_SIZE 1024
+
+int main(int argc, char **argv) {
+	FILE *input_file = fopen(argv[1],"r");
+	if (NULL == input_file) {
+		perror("Could not open file");
+		return 1;
+	}
+
+	//that is the stream we are going to pass information by to wc
+	FILE *output_stream = popen("wc -l","w");
+
+	char currentLine[CHUNCK_SIZE];
+	while (NULL != fgets(currentLine,CHUNCK_SIZE,input_file)) {
+		fputs(currentLine, output_stream);
+	}
+
+	pclose(output_stream);
+	fclose(input_file);
+	return 0;
+}
