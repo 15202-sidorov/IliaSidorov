5bf2a8c2f2319264a0d45293fdf535057705cf40
15202-sidorov <iliasidorov90@gmail.com>
2017-09-13 21:08:15 +0300

    Task 1 ready

--- Networks/task1/src/MulticastServer.java
+++ Networks/task1/src/MulticastServer.java
@@ -1,164 +1,138 @@
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.*;
 import java.util.*;
 
-class MulticastThread extends Thread {
+class MainThread extends Thread {
+    MainThread() {
+        message = new byte[256];
+    }
 
     public void run() {
-
         try {
-            IP = InetAddress.getLocalHost().getHostAddress();
-            group_address = InetAddress.getByName(GROUP_MULTICAST_ADDRESS);
-            group_socket = new MulticastSocket(GROUP_PORT);
-            group_list = new HashMap<InetAddress , Integer>();
-            message = new byte[256];
-            messageStr = null;
-
-            //setting timeout and joining multicast group
-            group_socket.setTimeToLive(TIMEOUT);
+            group_address = InetAddress.getByName("228.5.6.7");
+            group_port = 3000;
+            own_port = 3006;
+            group_socket = new MulticastSocket(group_port);
             group_socket.joinGroup(group_address);
+            String[] split;
+            copy_list = new LinkedList<InetSocketAddress>();
+            alive_list = new HashMap<InetSocketAddress, Boolean>();
 
-            sendRequest();
 
-            //setting timeout for socket
-            //that will make him check out whether the thread is interrupted or not
-            //after 5 seconds
-            group_socket.setSoTimeout(5000);
+            group_socket.setSoTimeout(TIMEOUT);
+            sendPing();
 
-            //making list of all others and waiting for someone to register or the oposite
-            while ( !this.isInterrupted() ) {
+            while ( !isInterrupted() ) {
                 try {
-                    group_socket.receive(datagram);
+                    split = receiveMessage();
                 }
                 catch ( SocketTimeoutException ex ) {
-                    if ( this.isInterrupted() ) {
-                        break;
-                    }
-                    else {
-                        continue;
-                    }
+                    sendPing();
+                    checkTimeout();
+                    continue;
                 }
-
-                String[] split =  recieveMessage();
-                switch ( split[0] ) {
-                    case REGISTER_STRING:
-                        if ( !group_list.containsKey( InetAddress.getByName(split[1]) )) {
-                            copies_amount++;
-                            System.out.println("New copy is found...");
-                            group_list.put(InetAddress.getByName(split[1]), GROUP_PORT);
-                            printWholeList();
-                            //telling the new member that we do exist by sending request message once more
-                            sendRequest();
-                        }
-                        break;
-                    case OUT_STRING:
-                        if ( group_list.containsKey(InetAddress.getByName(split[1])) ) {
-                            copies_amount--;
-                            System.out.println("Copy " +  split[1] + " is gone");
-                            group_list.remove(InetAddress.getByName(split[1]));
-                            printWholeList();
-                        }
-                        break;
-                    default:
-                        System.out.println("Could not recognize command " + split[0]);
-                }
+                makeChanges(split);
             }
 
-            sendLeaveRequest();
+
             group_socket.leaveGroup(group_address);
             group_socket.close();
+        }
+        catch (IOException ex) {
+            System.out.println("Could not send ping");
+        }
+    }
+
+    private void makeChanges( String[] split )  throws IOException{
+
+        switch ( split[0] ) {
+            case "PING":
+                InetSocketAddress socketAddress = new InetSocketAddress(InetAddress.getByName(split[1]), Integer.parseInt(split[2]));
+                if (copy_list.contains(socketAddress)) {
+                    alive_list.replace(socketAddress, true);
+                } else {
+                    copy_list.add(socketAddress);
+                    alive_list.put(socketAddress, true);
+                    printAllCopies();
+                }
+                break;
+            default:
+                System.out.println("Could not recongnize statement");
+                break;
 
         }
-        catch ( UnknownHostException ex ) {
-            System.out.println("Cannot find host");
-        }
-        catch ( IOException ex ) {
-            System.out.println("IOException from socket");
-        }
-        finally {
-            if ( !group_socket.isClosed() ) {
-                group_socket.close();
+    }
+
+    private void checkTimeout() {
+        Iterator<Map.Entry<InetSocketAddress, Boolean>> iter = alive_list.entrySet().iterator();
+        while( iter.hasNext() ) {
+            Map.Entry<InetSocketAddress, Boolean> item = iter.next();
+            if ( !item.getValue() ) {
+                copy_list.remove(item.getKey());
+                iter.remove();
+                printAllCopies();
+            }
+            else {
+                item.setValue(false);
             }
         }
     }
 
-    //configures the request datagram and sends it in multicast socket
-    private void sendRequest() throws IOException, UnknownHostException {
-        messageStr = REGISTER_STRING + " " + IP;
+    private void sendPing()  throws IOException {
+        messageStr = "PING " + InetAddress.getLocalHost().getHostAddress() + " " + own_port;
         message = messageStr.getBytes();
-        datagram = new DatagramPacket(message, message.length, InetAddress.getByName(GROUP_MULTICAST_ADDRESS), GROUP_PORT);
-
-        //multicasting to the group the fact the we exist;
-        group_socket.send(datagram);
-
+        packet = new DatagramPacket(message, message.length, group_address, group_port);
+        group_socket.send(packet);
     }
 
-    //configures out message to leave the group
-    private void sendLeaveRequest() {
-        messageStr = OUT_STRING + " " + IP;
-        message = messageStr.getBytes();
-        datagram.setData(message);
-        try {
-            group_socket.send(datagram);
+    private String[] receiveMessage() throws IOException, SocketTimeoutException {
+        group_socket.receive(packet);
+
+
+        return new String(packet.getData(),0,packet.getLength()).split(" ");
+    }
+
+    private void printAllCopies() {
+        System.out.println("Some changes are made");
+        Iterator<InetSocketAddress> iter = copy_list.iterator();
+        while ( iter.hasNext() ) {
+            InetSocketAddress item = iter.next();
+            System.out.println("IP : " + item.getAddress() + " Port : " + item.getPort());
         }
-        catch ( IOException ex ) {
-            System.out.println("Could not send OUT message");
-        }
-    }
-
-    private String[] recieveMessage() {
-        messageStr = new String(datagram.getData(), 0, datagram.getData().length);
-        return messageStr.split(" ");
     }
 
 
-    private void printWholeList() {
-        for ( Map.Entry<InetAddress, Integer > entry : group_list.entrySet() ) {
-            System.out.println("IP : " + entry.getKey().toString() + " Port : " +
-                    entry.getValue());
-        }
-        System.out.println("Copies in the net : " + copies_amount);
-    }
-
-    private InetAddress group_address;
-    private MulticastSocket group_socket;
-    private byte[] message;
     private String messageStr;
-    private DatagramPacket datagram;
-    private HashMap<InetAddress, Integer> group_list;
-    private int copies_amount = 0;
+    private byte[] message;
+    private MulticastSocket group_socket;
+    private InetAddress group_address;
+    private int group_port;
+    DatagramPacket packet;
+    private int own_port;
 
-    private String IP;
-    private final static String REGISTER_STRING = "REGISTER";
-    private final static String OUT_STRING = "OUT";
-    private final static String GROUP_MULTICAST_ADDRESS = "228.5.6.7";
-    private final static int GROUP_PORT = 4255;
-    private final static int TIMEOUT = 5;
+    private LinkedList< InetSocketAddress > copy_list;
+    private HashMap<InetSocketAddress, Boolean> alive_list;
+
+    static private int TIMEOUT = 3000;
+
+    static private String PING_STRING = "PING";
 
 }
+
 
 public class MulticastServer {
-    static public void main ( String args[] ) throws IOException {
-        MulticastThread mainThread = new MulticastThread();
-        mainThread.start();
-        BufferedReader commands = new BufferedReader( new InputStreamReader(System.in) );
-        while ( Objects.equals(commands.readLine(),EXIT_COMMAND) ) {
-
-        }
-        mainThread.interrupt();
-
-        System.out.println("Exit success");
+    static public void main( String[] args ) {
+        MainThread thread = new MainThread();
+        thread.start();
         try {
-            mainThread.join();
+            thread.join();
         }
         catch ( InterruptedException ex ) {
-            Thread.currentThread().interrupt();
+            thread.interrupt();
         }
+
+        return;
     }
 
-    private final static String EXIT_COMMAND = "exit";
 }
-
